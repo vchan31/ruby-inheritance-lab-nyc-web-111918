8fd26fc706b9b218e4430af7b733375da75a3938
Vincent Chan <vchan313@gmail.com>
2018-11-28 15:45:31 -0500

    did lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user.rb"
+
+
+class Student < User
+attr_reader :knowledge
+
+def initialize
+@knowledge = []
+end
+
+def learn(knowledge)
+@knowledge << knowledge
+end
+
+def knowledge
+@knowledge
+end
 
 end
